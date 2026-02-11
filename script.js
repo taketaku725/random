c968bf4f9755a56894e4b595db3dafa63e4f61d9
taketaku725 <115962176+taketaku725@users.noreply.github.com>
2026-02-11 21:20:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-import * as THREE from 'https://unpkg.com/three@0.158.0/build/three.module.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.158.0/+esm';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
+
 
 const suits = ["s", "h", "d", "c"];
 const ranks = ["a","2","3","4","5","6","7","8","9","10","j","q","k"];
@@ -195,10 +196,10 @@ function rollDice20() {
       if (result === 20) {
         d20.scale.set(1.4,1.4,1.4);
         setTimeout(() => d20.scale.set(1,1,1), 400);
+        isRolling20 = false;
       }
     }
   }
-  isRolling20 = false;
 
   requestAnimationFrame(spin);
 }
@@ -308,6 +309,7 @@ function shuffle(array) {
     [array[i], array[j]] = [array[j], array[i]];
   }
 }
+
 
 
 
